e38b487fc3a58c231bb6d4d430bf9363623ba279
Bowen Brinegar <bowenbrinegar@gmail.com>
2018-01-19 20:47:41 -0500

    show/hide saloon onclicks

---
+++ app/server/public/js/article.js
@@ -0,0 +1,9 @@
+$('#scrapedArticles').on('click', (e) => {
+  $('#articleContainer').toggle('display')
+  $('#bookmarked').toggle('display')
+});
+
+$('#bookmarkedArticles').on('click', (e) => {
+  $('#articleContainer').toggle('display')
+  $('#bookmarked').toggle('display')
+});
