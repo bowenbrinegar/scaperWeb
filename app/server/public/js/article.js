c49baeb1f5f8e1bc16c632dc93d9dd9cf74a6b0b
Bowen Brinegar <bowenbrinegar@gmail.com>
2018-01-21 13:10:31 -0500

    added css to bookmark container

---
+++ app/server/public/js/article.js
@@ -0,0 +1,39 @@
+$('#scrapedArticles').on('click', (e) => {
+  $('#articleContainer').toggle('display')
+  $('#bookmarked').toggle('display')
+});
+
+$('#bookmarkedArticles').on('click', (e) => {
+  $('#articleContainer').toggle('display')
+  $('#bookmarked').toggle('display')
+  fetchBookmarked()
+});
+
+$('#articleContainer').on('click', '#bookmark', function(e) {
+  e.stopPropagation();
+  let title = $(this).parent('div').children('h1').text()
+  let obj = {
+    title: title
+  };
+
+  $.ajax({
+    type: 'POST',
+    url: '/bookmark',
+    data: obj,
+    success: () => {
+      console.log('success')
+    }
+  })
+});
+
+const fetchBookmarked = () => {
+  $.ajax({
+    type: 'GET',
+    url: '/bookmarked',
+    success: renderBookmarked
+  })
+}
+
+const renderBookmarked = res => {
+  $('#bookmarked').html(res)
+}
