0ff8bf23f3e311db456204455ff3d67c528a20eb
Bowen Brinegar <bowenbrinegar@gmail.com>
2018-01-21 16:44:25 -0500

    reconfigured ajax; note refs

--- app/server/public/js/notes.js
+++ app/server/public/js/notes.js
@@ -1,14 +1,35 @@
-$('#bookmarkListItem').on('click', function() {
-  let id = $(this).attr('data-id')
+
+$('#bookmarked').on('click', '#note', function() {
+  $('#notes').toggle('display');
+  $('#mainContainer').css('filter', 'blur(20px)');
+  id = $(this).parent().attr('data-id');
+  fetchNotes(null)
+})
+
+const fetchNotes = function(res) {
   $.ajax({
     type: 'GET',
     url: '/article-notes/' + id,
     success: renderNotes
   })
-})
-
+};
 
 const renderNotes = res => {
-  console.log(res)
-  // $('#notes').html(res)
-}
+  $('#notes').html(res)
+};
+
+$('#notes').on('click', '#submitNote', function() {
+  console.log('working')
+  let note = {
+    article: id,
+    body: $('#noteInput').val()
+  };
+  $('#noteInput').val('')
+
+  $.ajax({
+    type: 'POST',
+    url: '/note',
+    data: note,
+    success: fetchNotes
+  })
+});
