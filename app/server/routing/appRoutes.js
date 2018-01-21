729e78b783b64583c1ee98e5ba65d90dd197626f
Bowen Brinegar <bowenbrinegar@gmail.com>
2018-01-21 14:09:49 -0500

    initial notes file structure

--- app/server/routing/appRoutes.js
+++ app/server/routing/appRoutes.js
@@ -1,14 +1,18 @@
 
 module.exports = (app, cheerio, request, db) => {
   app.get('/', (req, res) => {
+    // db.Article.remove({}, {multi: true})
+    //   .then( result => console.log('success'))
+    //   .catch( err => console.log(err));
     res.render('index')
   });
 
   app.get('/scrape', (req, res) => {
     const results =  [];
-    request("https://medium.com/", (error, response, html) => {
-      const $ = cheerio.load(html);
+    request("https://medium.com/",
+      (error, response, html) => {
 
+      const $ = cheerio.load(html);
       $("h3").each(function (i, element) {
         let title = $(this).text();
         results.push({
@@ -16,55 +20,42 @@ module.exports = (app, cheerio, request, db) => {
         });
       });
 
-      res.render('partials/articles/index', {layout: false, articles: results})
+      res.render('partials/articles/index',
+        {layout: false, articles: results})
     });
   });
 
   app.get('/bookmarked', (req, res) => {
-
-    // db.Article.remove({title: ''}, {multi: true}).then(() => {
-    //   console.log('success')
-    // }).catch(err => {
-    //   console.log(err)
-    // })
-
     db.Article.find({})
       .then( (result) => {
-        console.log('bookmarked res', result)
-        res.render('partials/bookmarked/index', {layout: false, articles: result})
+        res.render('partials/bookmarked/index',
+          {layout: false, articles: result})
       })
-      .catch( (err) => {
-        console.log(err)
-      })
+      .catch( err => console.log(err))
   });
 
   app.post('/bookmark', (req, res) => {
-    console.log("bookmark", req.body)
     db.Article.create(req.body)
-      .then( (result) => {
-        console.log("bookmarked result", result)
-        res.send('success')
-      })
-      .catch( (err) => {
-        console.log(err)
-      })
+      .then( result => res.send('success'))
+      .catch( err => console.log(err))
   });
 
   app.get('/article-notes/:id', (req, res) => {
     db.Article.findOne({_id: req.params.id})
       .populate('note')
-      .then( (result) => { res.send(result) } )
-      .catch( (err) => { res.send(err) } )
+      .then( result => res.send(result))
+      .catch( err => res.send(err))
   });
 
   app.post('/note', (req, res) => {
     const note = req.body
     console.log('note', note)
     db.Note.create(note)
-      .then( (note) => {
+      .then( note => {
         db.Article.findByIdAndUpdate(note.id, {note: note._id})
-          .then( (note) => { res.json(note) } )
-          .catch( (err) => { res.send(err) } )
+          .then( note => res.json(note))
+          .catch( err => res.send(err))
       })
+      .catch( err => console.log(err))
   })
 };
